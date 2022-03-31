47feb5f4e0dcb6dff8db82077327d3f18d2472e8
cjcq <425430798@qq.com>
2022-03-31 22:16:07 +0800

    Initial commit

---
+++ src/main/java/cj/springboot/template/rabbitmqtemplate/RabbitmqtemplateApplication.java
@@ -0,0 +1,13 @@
+package cj.springboot.template.rabbitmqtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqtemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqtemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cj/springboot/template/rabbitmqtemplate/RabbitmqtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package cj.springboot.template.rabbitmqtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqtemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
